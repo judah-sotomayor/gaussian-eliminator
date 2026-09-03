b210a0e5814271398d6174ab779220d967c5d90d
Judah Sotomayor <jisotomayor@liberty.edu>
2026-09-03 10:15:37 -0400

    feat: Update flake to use flake-parts.
    Adapt the flake to my more standard template.

--- flake.nix
+++ flake.nix
@@ -1,34 +1,69 @@
 {
-  description = "gaussian-eliminator";
+  description = "gaussian eliminator software";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    cl-nix-lite.url = "github:judah-sotomayor/cl-nix-lite/v0";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    cl-nix-lite.url = "github:hraban/cl-nix-lite/v0";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    in-nomine = {
+      url = "github:judah-sotomayor/in-nomine";
+      flake = false;
+    };
   };
 
-  outputs = { nixpkgs, cl-nix-lite, ... }:
-  let
-    allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    lib = nixpkgs.lib;
-    forAllSystems = f: lib.genAttrs allSystems
-      (system: f {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            cl-nix-lite.overlays.default
-          ];
-        };
-      });
+  outputs =
+    inputs@{ flake-parts, cl-nix-lite, ... }:
 
-  in {
-    devShells = forAllSystems ({pkgs}:  {
-      default = with pkgs.lispPackagesLite;
-        lispDerivation {
-          src = pkgs.lib.cleanSource ./.;
-          lispSystems = [ "gaussian-eliminator" ];
-          lispDependencies = [ serapeum ];
-          lispCheckDependencies = [ fiasco ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks.flakeModule
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ cl-nix-lite.overlays.default ];
+          };
+
+          treefmt = {
+            programs.nixfmt.enable = true;
+            programs.nixfmt.strict = true;
+          };
+
+          packages = {
+            gaussian-eliminator =
+              with pkgs.lispPackagesLite;
+              lispDerivation {
+                src = pkgs.lib.cleanSource ./.;
+                lispSystems = [ "gaussian-eliminator" ];
+                lispDependencies = [ serapeum ];
+                lispCheckDependencies = [ fiasco ];
+              };
+          };
+
+          devShells.default = pkgs.mkShell {
+            shellHook = ''
+              ${config.pre-commit.shellHook}
+            '';
+            packages = config.pre-commit.settings.enabledPackages;
+          };
         };
-    });
-  };
+    };
 }
